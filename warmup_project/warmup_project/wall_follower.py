6158bf652fa2c6a9f3bb27336236a134e9612611
Alex Matsoukas <amatsoukas@olin.edu>
2023-09-11 20:44:00 -0400

    add data points for more robust scan

--- warmup_project/warmup_project/wall_follower.py
+++ warmup_project/warmup_project/wall_follower.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from neato2_interfaces.msg import Bump
@@ -13,17 +14,19 @@ class WallFollowerNode(Node):
         self.velocity_pub = self.create_publisher(Twist, 'cmd_vel', 10)
         self.scan_sub = self.create_subscription(
             LaserScan, 'scan', self.process_data, 10)
+        self.marker_pub = self.create_publisher(
+            Marker, 'marker', 10)
         self.create_subscription(Bump, 'bump', self.process_bump, 10)
 
         self.estop = False
 
-        self.theta1 = 0.0
+        self.theta1 = None
         self.base_ang_vel = pi / 4
 
         self.create_timer(.1, self.run_loop)
 
     def run_loop(self):
-        if not self.estop:
+        if not self.estop and self.theta1 is not None:
             msg = Twist()
             msg.linear.x = 0.25
             msg.angular.z = self.base_ang_vel * ((self.theta1 - 45) / 45)
@@ -33,15 +36,24 @@ class WallFollowerNode(Node):
 
     def process_data(self, scan_data):
         # pick a few angles to calculate
-        angs = [215, 220, 225, 230, 235]
+        angs = [180, 185, 190, 195, 200, 205, 210, 215, 220,
+                225, 230, 235, 240, 245, 250, 255, 260, 265, 270]
 
         res = []
         # iterate through and calculate the angle with the wall
         for ang in angs:
-            theta = degrees(
-                atan(scan_data.ranges[ang] / scan_data.ranges[ang + 90]))
-            res.append(theta - 225 + ang)  # account for difference in angles
-        self.theta1 = mean(res)
+            x = scan_data.ranges[ang]
+            y = scan_data.ranges[ang + 90]
+            if x != 0 and y != 0:
+                theta = degrees(
+                    atan(x / y))
+                # account for difference in angles
+                res.append(theta - 225 + ang)
+        print(res)
+        self.theta1 = None if len(res) == 0 else mean(res)
+        print(self.theta1)
+
+        # publish points along the wall for visual
 
     def process_bump(self, bump_data):
         if bump_data.left_front or bump_data.right_front or bump_data.left_side or bump_data.right_side:
