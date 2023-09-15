91ca5d8c37ffcc54c29bf3a37ed7956bfaa52b18
Alex Matsoukas <amatsoukas@olin.edu>
2023-09-15 19:18:42 -0400

    add file for person follower

--- warmup_project/setup.py
+++ warmup_project/setup.py
@@ -23,7 +23,8 @@ setup(
             'marker = warmup_project.marker:main',
             'teleop = warmup_project.teleop:main',
             'drive_square = warmup_project.drive_square:main',
-            'wall_follower = warmup_project.wall_follower:main'
+            'wall_follower = warmup_project.wall_follower:main',
+            'person_follower = warmup_project.person_follower:main'
         ],
     },
 )
