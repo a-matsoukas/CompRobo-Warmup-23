bbfb23ee0a2f51351e5f27b8a4e9f98eb7ef5377
Alex Matsoukas <amatsoukas@olin.edu>
2023-09-15 21:29:46 -0400

    tweak parameters to optimize for physical neato and add beginner code for rviz marker

--- warmup_project/warmup_project/person_follower.py
+++ warmup_project/warmup_project/person_follower.py
@@ -1,11 +1,10 @@
 import rclpy
 from rclpy.node import Node
 from visualization_msgs.msg import Marker
-from geometry_msgs.msg import Twist, Point
+from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from neato2_interfaces.msg import Bump
 from math import pi, atan, sin, cos, degrees, radians, isinf
-from statistics import mean
 
 
 class PersonFollowerNode(Node):
@@ -23,9 +22,20 @@ class PersonFollowerNode(Node):
         self.target_angle = None
         self.target_dis = None
 
-        self.left_ang = 45
-        self.follow_dist = 0.5
-        self.max_object_range = 1.0
+        self.target_pt = Marker()
+        self.target_pt.header.frame_id = 'odom'
+        self.target_pt.type = 3
+        self.target_pt.scale.x = 0.25
+        self.target_pt.scale.y = 0.25
+        self.target_pt.scale.z = 1.0
+        self.target_pt.color.a = 1.0
+        self.target_pt.color.r = 0.0
+        self.target_pt.color.g = 1.0
+        self.target_pt.color.b = 0.0
+
+        self.left_ang = 30
+        self.follow_dist = 0.75
+        self.max_object_range = 1.25
 
         self.base_ang_vel = pi / 4
         self.base_lin_vel = 0.5
@@ -47,6 +57,8 @@ class PersonFollowerNode(Node):
             msg.angular.z = self.base_ang_vel * \
                 (self.target_angle / self.left_ang)
             self.velocity_pub.publish(msg)
+            self.pub_marker(self.polar_to_cart(
+                self.target_dis, self.target_angle))
         else:
             self.stop_moving()
 
@@ -80,6 +92,18 @@ class PersonFollowerNode(Node):
             self.target_dis = None
             self.target_angle = None
         print(self.target_dis, self.target_angle)
+
+    def polar_to_cart(self, rad, theta):
+        x = rad * cos(radians(theta))
+        y = rad * sin(radians(theta))
+        z = 0.0
+        return [x, y, z]
+
+    def pub_marker(self, coords):
+        self.target_pt.pose.position.x = coords[0]
+        self.target_pt.pose.position.y = coords[1]
+        self.target_pt.pose.position.z = coords[2]
+        self.marker_pub.publish(self.target_pt)
 
     def process_bump(self, bump_data):
         if bump_data.left_front or bump_data.right_front or bump_data.left_side or bump_data.right_side:
